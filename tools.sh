e6ed6a0869c9bf823c5f62c7b5da8dd5f713fd54
karrrtik007 <74298752+karrrtik007@users.noreply.github.com>
2021-09-18 19:00:53 +0530

    Update tools.sh

--- tools.sh
+++ tools.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 
-printf "MAKE SURE YOU HAVE CREATE 'tools' FOLDER IN HOME DIRECTORY"
+
+echo "create tools directory and place script inside "
 sleep 4
 echo ""
 echo ""
 
 echo "installing requirements"
-sudo apt-get -y install python3 golang git python-pip amass
-echo "done________________________________________________________________________________________________________________________________________________________________________________"
+sudo apt-get -y install python3 golang git python3-pip
+echo "done"
 
 echo "cloning Arjun"
 git clone https://github.com/s0md3v/Arjun.git
-echo "done________________________________________________________________________________________________________________________________________________________________________________"
+echo "done"
 
 echo "cloning fuzzdb"
 git clone https://github.com/fuzzdb-project/fuzzdb.git
-echo "done________________________________________________________________________________________________________________________________________________________________________________"
+echo "done"
 
 echo "cloning impacket"
 git clone https://github.com/SecureAuthCorp/impacket.git
-echo "done________________________________________________________________________________________________________________________________________________________________________________"
+echo "done"
 
 echo "cloning paramspider"
 git clone https://github.com/devanshbatham/ParamSpider.git
-echo "done________________________________________________________________________________________________________________________________________________________________________________"
+echo "done"
 
 echo "cloning seclists"
 git clone https://github.com/danielmiessler/SecLists.git
-echo "done________________________________________________________________________________________________________________________________________________________________________________"
+echo "done"
 
 echo "cloning gobuster"
 wget https://github.com/OJ/gobuster/releases/download/v3.1.0/gobuster-linux-amd64.7z
@@ -37,20 +38,34 @@ echo "done, please move gobuster to /usr/local/bin______________________________
 
 echo "cloning anew"
 go get -u github.com/tomnomnom/anew
-mv ~/go/bin/anew ~/tools
-echo "done________________________________________________________________________________________________________________________________________________________________________________"
+echo "done"
 
 echo "cloning httprobe"
 go get -u github.com/tomnomnom/httprobe
-mv ~/go/bin/httprobe ~/tools
-echo "done________________________________________________________________________________________________________________________________________________________________________________"
+echo "done"
 
 echo "cloning assetfinder"
 go get -u github.com/tomnomnom/assetfinder
-mv ~/go/bin/assetfinder ~/tools
-echo "done________________________________________________________________________________________________________________________________________________________________________________"
+echo "done"
 
 echo "cloning waybackurls"
 go get github.com/tomnomnom/waybackurls
-mv ~/go/bin/assetfinder ~/tools
-echo "done________________________________________________________________________________________________________________________________________________________________________________"
+echo "done"
+
+echo "cloning ffuf"
+go get -u github.com/ffuf/ffuf
+echo "done"
+
+echo "cloning amass"
+wget https://github.com/OWASP/Amass/releases/download/v3.13.4/amass_linux_amd64.zip
+unzip amass_linux_amd64.zip
+rm -rf amass_linux_amd64.zip
+echo"done"
+
+echo"cloning dnsgen"
+https://github.com/ProjectAnte/dnsgen.git
+echo "done"
+
+
+echo "getting repository tools"
+sudo apt install sqlmap wireshark 
